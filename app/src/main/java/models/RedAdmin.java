94e564d054e52c8bb365a514056ba6d6759a3ed3
Angel24U <garridoadamsangel@gmail.com>
2024-12-03 19:31:19 -0300

    modificacion de codigo de la base de datos!

--- app/src/main/java/models/RedAdmin.java
+++ app/src/main/java/models/RedAdmin.java
@@ -6,7 +6,7 @@ import java.net.UnknownHostException;
 public class RedAdmin {
 
     // Variable estática para mantener la IP sel servidor de servicios
-    private static String iPLocal = "192.168.1.65";
+    private static String iPLocal = "10.110.45.118";
 
     // Método para establecer IP
     public  void setIpLocal(String iPRecibida) {
